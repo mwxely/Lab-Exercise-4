6b88620e14b697b63e8e817e0640471383bb6fbc
Alex <alex@linruochendeMacBook-Pro.local>
2019-10-03 21:40:59 -0600

    Add

--- Lab_exercise_4/app/src/main/java/com/example/lab_exercise_4/Rectangle.java
+++ Lab_exercise_4/app/src/main/java/com/example/lab_exercise_4/Rectangle.java
@@ -1,4 +1,12 @@
 package com.example.lab_exercise_4;
 
+
+
 public class Rectangle extends Shape {
+    private String color = "blue";
+
+    public Rectangle(int x, int y, String color) {
+        super(x, y);
+        this.color = color;
+    }
 }
