0bb34a063f7e82afe64384f864c3c6f11eb0935f
AlexRuochenLin <46505421+AlexRuochenLin@users.noreply.github.com>
2019-10-03 21:46:54 -0600

    Update Rectangle.java

--- Lab_exercise_4/app/src/main/java/com/example/lab_exercise_4/Rectangle.java
+++ Lab_exercise_4/app/src/main/java/com/example/lab_exercise_4/Rectangle.java
@@ -3,10 +3,10 @@ package com.example.lab_exercise_4;
 
 
 public class Rectangle extends Shape {
-    private String color = "blue";
+    
 
-    public Rectangle(int x, int y, String color) {
+    public Rectangle(int x, int y) {
         super(x, y);
-        this.color = color;
+       
     }
 }
