fb0de65591a055b1bb410dd2e45fd50e6f4fc296
Holmes <Holmes@Holmes.local>
2019-10-03 20:32:24 -0600

    Square Done

---
+++ Lab_exercise_4/app/src/main/java/com/example/lab_exercise_4/Square.java
@@ -0,0 +1,11 @@
+package com.example.lab_exercise_4;
+
+public class Square extends Shape{
+
+    private String color = "Green";
+
+    public Square(int x, int y, String color) {
+        super(x, y);
+        this.color = color;
+    }
+}
