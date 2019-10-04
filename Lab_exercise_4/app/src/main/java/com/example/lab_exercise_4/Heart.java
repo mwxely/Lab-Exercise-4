2fe74708c8c80b267eca5c45d2b0e056bb74ae69
Chengzhang Dong <chengzha@ualberta.ca>
2019-10-03 21:17:00 -0600

    aaaaaa

--- Lab_exercise_4/app/src/main/java/com/example/lab_exercise_4/Heart.java
+++ Lab_exercise_4/app/src/main/java/com/example/lab_exercise_4/Heart.java
@@ -1,4 +1,7 @@
 package com.example.lab_exercise_4;
 
 public class Heart extends Shape {
+    public Heart(int x, int y) {
+        super(x, y);
+    }
 }
--- Lab_exercise_4/app/src/main/java/com/example/lab_exercise_4/Shape.java
+++ Lab_exercise_4/app/src/main/java/com/example/lab_exercise_4/Shape.java
@@ -1,12 +1,11 @@
 package com.example.lab_exercise_4;
 
 public class Shape {
-
-    int x;
-    int y;
-
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
     }
+
+    int x;
+    int y;
 }
